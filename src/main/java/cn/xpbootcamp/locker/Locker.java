168dc9abdce624ae573fd065692022f7c55e8c5f
Hairui guo <hairui.guo@thoughtworks.com>
2020-08-29 18:46:34 +0800

    [hairui] Return a ticket when deposit bag while locker is not empty.

--- src/main/java/cn/xpbootcamp/locker/Locker.java
+++ src/main/java/cn/xpbootcamp/locker/Locker.java
@@ -2,13 +2,34 @@ package cn.xpbootcamp.locker;
 
 public class Locker {
     public static final String DEPOSIT_BAG_FAILED = "存包失败！";
+    public static final String DEPOSIT_BAG_TICKET = "小票";
     private int idleNum;
 
     Locker(int idleNum) {
         this.idleNum = idleNum;
     }
 
+    public int getIdleNum() {
+        return idleNum;
+    }
+
+    public void setIdleNum(int idleNum) {
+        this.idleNum = idleNum;
+    }
+
+    public void addIdleNum(int num) {
+        setIdleNum(getIdleNum() - 1);
+    }
+
     public String depositBag() {
-        return DEPOSIT_BAG_FAILED;
+        String result = null;
+        if (idleNum == 0) {
+            result = DEPOSIT_BAG_FAILED;
+        }
+        if (idleNum > 0) {
+            addIdleNum(-1);
+            result = DEPOSIT_BAG_TICKET;
+        }
+        return result;
     }
 }
--- src/test/java/cn/xpbootcamp/locker/LockerTest.java
+++ src/test/java/cn/xpbootcamp/locker/LockerTest.java
@@ -13,4 +13,11 @@ public class LockerTest {
         assertEquals(Locker.DEPOSIT_BAG_FAILED, depositBagResult);
     }
 
+    @Test
+    public void should_return_ticket_when_deposit_bag_given_locker_is_not_empty() {
+        Locker locker = new Locker(1);
+        String depositBagResult = locker.depositBag();
+        assertEquals(Locker.DEPOSIT_BAG_TICKET, depositBagResult);
+    }
+
 }
